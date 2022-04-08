e2f3f30588bcedd62f7a95e1a488831803f1ac7d
EP <kjuioqq8@gmail.com>
2022-04-08 09:34:13 +0900

    서블릿 등록

--- src/main/java/com/learning/spring_mvc/SpringMvcApplication.java
+++ src/main/java/com/learning/spring_mvc/SpringMvcApplication.java
@@ -2,12 +2,14 @@ package com.learning.spring_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/** 서블릿 컴포넌트 스캔 (서블릿 자동 등록) 하위 패키지의 서블릿을 찾아서 실행할 수 있도록 도와준다. **/
+@ServletComponentScan
 @SpringBootApplication
 public class SpringMvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcApplication.class, args);
     }
-
 }
